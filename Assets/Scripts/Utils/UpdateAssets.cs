247021d6a5fea005e23438d98e700fb3235b7e8b
Mark Kashchaev <unihilist@ya.ru>
2021-12-20 10:54:47 +0300

    Подготовил материалы для холла, для дальнейшей доработки

---
+++ Assets/Scripts/Utils/UpdateAssets.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class UpdateAssets : MonoBehaviour
+{
+    [SerializeField] private bool _updateOn;
+    private double time;
+    [EditorButton]
+    void UpdateOk()
+    {
+        if (!_updateOn)
+        {
+            EditorApplication.update += UpdateE;
+        }
+        else
+        {
+            EditorApplication.update -= UpdateE;
+        }
+        _updateOn = !_updateOn;
+    }
+
+    private void UpdateE()
+    {
+        if (EditorApplication.timeSinceStartup > time)
+        {
+            AssetDatabase.Refresh();
+            Debug.Log("xxx");
+            time = EditorApplication.timeSinceStartup + 2;
+        }
+    }
+}
